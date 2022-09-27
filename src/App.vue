8ccbaea06bd6b4bfd5e7e2e11ad5ef101f005a93
Tornike Jandieri <108745129+tornikejandieri@users.noreply.github.com>
2022-09-27 23:15:30 +0400

    name prop of transition component

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <button @click="animateBlock">Animate</button>
   </div>
   <div class="container">
-    <transition>
+    <transition name="para">
       <p v-if="paraIsVisible">this is only sometimes visible...</p>
     </transition>
 
@@ -93,30 +93,30 @@ button:active {
   animation: slide-fade 0.3s ease-out forwards;
 }
 
-.v-enter-from {
+.para-enter-from {
   opacity: 0;
   transform: translateY(-30px);
 }
 
-.v-enter-active {
+.para-enter-active {
   transition: all 0.3s ease-out;
 }
 
-.v-enter-to {
+.para-enter-to {
   opacity: 1;
   transform: translateY(0);
 }
 
-.v-leave-from {
+.para-leave-from {
   opacity: 1;
   transform: translateY(0);
 }
 
-.v-leave-active {
+.para-leave-active {
   transition: all 0.3s ease-in;
 }
 
-.v-leave-to {
+.para-leave-to {
   opacity: 0;
   transform: translateY(30px);
 }
